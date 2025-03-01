bb914f8461c95a0aada329cd3daee9af70dc6eda
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-03-01 17:09:47 -0300

    feat(setup): create the connection pool for the app

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 )
 
@@ -17,5 +20,45 @@ func main() {
 
 	dsn := os.Getenv("DB_DSN")
 
-	fmt.Println("Hello, GoTodo")
+	if dsn == "" {
+		log.Fatal("required DB_DSN env var missing")
+		os.Exit(1)
+	}
+
+	db, err := openDB(dsn)
+	if err != nil {
+		log.Fatal("failed to open db connection pool")
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
+	fmt.Println("db connection established")
+}
+
+func openDB(dsn string) (*pgxpool.Pool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	poolConfig, err := pgxpool.ParseConfig(dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	poolConfig.MaxConns = 25
+	poolConfig.MinConns = 6 // use ~25% of MaxConns
+	poolConfig.MaxConnIdleTime = 15 * time.Minute
+
+	connectionPool, err := pgxpool.NewWithConfig(ctx, poolConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	err = connectionPool.Ping(ctx)
+	if err != nil {
+		connectionPool.Close()
+		return nil, err
+	}
+
+	return connectionPool, nil
 }
