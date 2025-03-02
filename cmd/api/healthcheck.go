63455ba479cf093fa6967c976eedda2bb858a317
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-03-02 11:30:07 -0300

    feat(json responses): start using envlope to serve more structured data

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -11,7 +11,7 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 		"version":     version,
 	}
 
-	err := app.writeJSON(w, http.StatusOK, data, nil)
+	err := app.writeJSON(w, http.StatusOK, envelope{"server_info": data}, nil)
 	if err != nil {
 		app.logger.Error(err.Error())
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
