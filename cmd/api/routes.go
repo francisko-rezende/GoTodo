ba23ea7afd01a37537b31b27888661937dfc1539
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-03-03 08:51:39 -0300

    feat(error responses): centralize error responses and use custom ones for method not allowed and not found

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,6 +9,9 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
 	return router
