f8a818ae58b22cfa6077ac0bf1f5179af382cfdf
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-04-17 22:49:52 -0300

    feat: create middleware for checking for auth header and retrieving user info/passing it to context

---
+++ cmd/api/middleware.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"GoTodo/internal/data"
+	"GoTodo/internal/data/validator"
+	"errors"
+	"net/http"
+	"strings"
+)
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Vary", "Authorization")
+
+		authorizationHeader := r.Header.Get("Authorization")
+
+		if authorizationHeader == "" {
+			app.invalidAuthenticationHeaderResponse(w, r)
+			return
+		}
+
+		headerParts := strings.Split(authorizationHeader, " ")
+		if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+			app.invalidAuthenticationHeaderResponse(w, r)
+			return
+		}
+
+		token := headerParts[1]
+
+		v := validator.New()
+
+		if data.ValidateTokenPlainText(v, token); !v.Valid() {
+			app.invalidAuthenticationHeaderResponse(w, r)
+			return
+		}
+
+		user, err := app.models.Tokens.GetForToken(token)
+		if err != nil {
+			switch {
+			case errors.Is(err, data.ErrRecordNotFound):
+				app.invalidAuthenticationHeaderResponse(w, r)
+			default:
+				app.serverErrorResponse(w, r, err)
+			}
+
+			return
+		}
+
+		r = app.contextSetUser(r, user)
+		next.ServeHTTP(w, r)
+	})
+}
